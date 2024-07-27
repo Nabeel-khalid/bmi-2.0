f1896a988488beb9443ba64a9924cb8a2dcb562d
Nabeel-khalid <nabeel.khald03@gmail.com>
2024-07-27 14:34:50 +0100

    Update SVG Body.py

--- pages/SVG Body.py
+++ pages/SVG Body.py
@@ -1,5 +1,8 @@
 import streamlit as st
-from streamlit.components.v1 import components
+import streamlit.components.v1 as components
+
+components.html("example")
+
 
 def calculate_bmi(weight, height):
     return weight / (height / 100) ** 2
